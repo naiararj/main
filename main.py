b4540aff78d20552a7e5fa5e7188b84973a4865c
naiararj <102701022+naiararj@users.noreply.github.com>
2022-04-22 00:45:40 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+n= str(input('Digite seu nome completo:')) . strip()
+nome = n. split()
+print('Seu primeiro nome é {}' . format(nome[0]))
+salário = (input('Salário é: R$'))
+print('Qual é o salário do mês de abril?')
+print('O salário do mês de abril de Ana doi de R$ 2.000,00')
+
+
+
